7d3b2e2f4104247492563c11f78a506cec60bc2f
Pesto <Pesto@macbook-air-2.myfiosgateway.com>
2021-02-11 18:02:51 -0500

    fixed modal

--- src/pages/life.js
+++ src/pages/life.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ImageGrid from '../components/imagegrid';
 import Layout from '../components/layout'
-import Modal from '../components/Modal';
+import Modal from '../components/modal';
 import SEO from '../components/seo'
 
 const Life = (props) => {

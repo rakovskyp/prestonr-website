9f98b4d91a57836e3ebad78e7a97a4f47c4ac87e
Pesto <Pesto@MacBook-Air-2.fios-router.home>
2021-12-17 17:53:33 -0500

    updating resume

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,25 +1,49 @@
 import React from "react"
 
-import Layout from '../components/layout'
-import SEO from '../components/seo'
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
-
-const Contact = (props) => {
-
-    return (
-        <Layout>
-            <SEO title="Contact" />
-            <div className='fade-in'>
-                <h1 className='title shrink'>contact</h1>
-                <p>
-                    <a className='links' href='mailto:rakovsky.p@northeastern.edu'>Email</a> / 
-                    <a className='links'href='https://www.linkedin.com/in/rakovskypreston/' target='_blank'> LinkedIn</a> / 
-                    <a className='links'href='https://github.com/rakovskyp' target='_blank'> Github</a> /
-                    <a className='links'href='https://prestonr.xyz/latestresume.pdf' target='_blank'> Resume</a>
-                </p>
-            </div>
-        </Layout>
-    )
+const Contact = props => {
+  return (
+    <Layout>
+      <SEO title="Contact" />
+      <div className="fade-in">
+        <h1 className="title shrink">contact</h1>
+        <p>
+          <a className="links" href="mailto:rakovsky.p@northeastern.edu">
+            Email
+          </a>{" "}
+          /
+          <a
+            className="links"
+            href="https://www.linkedin.com/in/rakovskypreston/"
+            target="_blank"
+          >
+            {" "}
+            LinkedIn
+          </a>{" "}
+          /
+          <a
+            className="links"
+            href="https://github.com/rakovskyp"
+            target="_blank"
+          >
+            {" "}
+            Github
+          </a>{" "}
+          /
+          <a
+            className="links"
+            href="https://prestonr.xyz/resume.pdf"
+            target="_blank"
+          >
+            {" "}
+            Resume
+          </a>
+        </p>
+      </div>
+    </Layout>
+  )
 }
 
-export default Contact;
+export default Contact
